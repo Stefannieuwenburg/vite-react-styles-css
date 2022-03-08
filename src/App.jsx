3ecdacb376c9e888ace3912fc948339724d23b1f
Stefan Nieuwenburg <nieuwenburg.stefan@gmail.com>
2022-03-08 13:51:36 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+// import { Component} from "react"
 import Header from "./components/Header";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
 
+//functional component nieuwe manier van react
 export default function App() {
-  
     return (
         <div className="App">
             <Header />
@@ -12,3 +13,24 @@ export default function App() {
         </div>
     );
 }
+
+
+
+//class component woord niet meer gebruikt in modern style
+
+// export default class App extends Component {
+    
+//     render() { 
+//         return (
+//             <div className="App">
+//                  <Header />
+//                  <Content />
+//                  <Footer />
+//             </div>
+//         );
+//     }
+// }
+ 
+
+
+
